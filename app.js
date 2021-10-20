d3a1b00a4cc8432f9ccfb27ca17586c2803f96ed
Jyoti Bisht (Joe) <jyotibisht6114@gmail.com>
2021-10-20 18:40:06 +0530

    trasnsferring here and there

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 const recipeRouter = require("./routes/recipes")
 const methodOverride = require("method-override")
 
+const host = '0.0.0.0';
+const port = process.env.port || 3000;
+const server = app.listen(port, host,()=>{
+    console.log("app running on specified port and host");
+});
+
 const DATABASE = 'mongodb+srv://joeyous:fgbjjgci@cluster0.34ov6.mongodb.net/recipesApp?retryWrites=true&w=majority'
 mongoose.connect(DATABASE,{
         useNewUrlParser: true,
@@ -32,11 +38,5 @@ app.get("/", async (req, res)=>{
 })
 app.use("/recipes", recipeRouter)
 
-
-const host = '0.0.0.0';
-const port = process.env.port || 3000;
-const server = app.listen(port, host,()=>{
-    console.log("app running on specified port and host");
-});
 
 // app.listen(3000);
