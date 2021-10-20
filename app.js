132fd6d99d28fb8738a185363abf9d9fc677cce4
Jyoti Bisht (Joe) <jyotibisht6114@gmail.com>
2021-10-20 20:10:34 +0530

    m

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const recipeRouter = require("./routes/recipes")
 const methodOverride = require("method-override")
 
 const host = '0.0.0.0';
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 const server = app.listen(port, host,()=>{
     console.log("app running on specified port and host");
 });
@@ -31,20 +31,14 @@ app.use(express.urlencoded({extended:false}))
 app.use(methodOverride("_method"))
 // -----
 
-const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
-app.listen(PORT, LOCAL_ADDRESS, () => {
-  const address = app.address();
-  console.log('server listening at', address);
-});
 
-
-// app.get("/", async (req, res)=>{
-//     const recipes = await Recipee.find().sort({
-//         createdAt: 'desc'
-//     })
-//     res.render("recipes/index", {recipes : recipes});
-// })
-// app.use("/recipes", recipeRouter)
+app.get("/", async (req, res)=>{
+    const recipes = await Recipee.find().sort({
+        createdAt: 'desc'
+    })
+    res.render("recipes/index", {recipes : recipes});
+})
+app.use("/recipes", recipeRouter)
 
 
 // app.listen(3000);
